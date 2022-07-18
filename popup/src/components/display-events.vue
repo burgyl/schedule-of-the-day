001e2ea39d6bd5696f92388072d9fe2ad209c753
Lucas Bürgy <burgyl@users.noreply.github.com>
2022-07-18 10:44:49 +0200

    Test vue-googleapis

--- popup/src/components/display-events.vue
+++ popup/src/components/display-events.vue
@@ -1,6 +1,23 @@
 <template>
-  <div>
-    My event
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>isInit: {{ isInit }}</p>
+    <p v-if="user && isSignedIn">{{ user.getBasicProfile().getName() }}</p>
+    <button
+      :disabled="!isInit || isSignedIn"
+      @click="signIn"
+    >SignIn
+    </button>
+    <button
+      :disabled="!isInit || !isSignedIn"
+      @click="signOut"
+    >SignOut
+    </button>
+    <button
+      :disabled="!isInit || !isSignedIn"
+      @click="disconnect"
+    >Disconnect
+    </button>
   </div>
 </template>
 
@@ -13,5 +30,47 @@ export default {
 <script setup lang="ts">
 import { ref } from 'vue'
 
-const value = ref('');
+const isInit = ref<boolean>(false);
+const isSignedIn = ref<boolean>(false);
+const user = ref(null);
+
+const checkGALoad = setInterval(function () {
+  this.isInit = this.$google.isInit
+  if (this.isInit) {
+    this.isSignedIn = this.$google.api.auth2.getAuthInstance().isSignedIn.get()
+    this.$google.api.auth2.getAuthInstance().isSignedIn.listen(this.signInListener)
+    this.$google.api.auth2.getAuthInstance().currentUser.listen(this.userListener)
+    clearInterval(checkGALoad)
+  }
+}, 200);
+
+const signIn = async () => {
+  try {
+    return await this.$google.api.auth2.getAuthInstance().signIn()
+  } catch (e) {
+    console.log(e)
+  }
+
+};
+
+const signOut = async () => {
+  try {
+    return await this.$google.api.auth2.getAuthInstance().signOut()
+  } catch (e) {
+    console.log(e)
+  }
+
+};
+
+const disconnect = async () => {
+  return await this.$google.api.auth2.getAuthInstance().disconnect()
+};
+
+const signInListener = (status) => {
+  this.isSignedIn = status;
+};
+const userListener = (user) => {
+  this.user = user;
+};
+
 </script>
--- popup/src/main.ts
+++ popup/src/main.ts
@@ -4,11 +4,19 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import 'element-plus/theme-chalk/dark/css-vars.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import Gapi from 'vue-googleapis'
 import App from './App.vue'
 
 const app = createApp(App);
 
 app.use(ElementPlus);
 Object.entries(ElementPlusIconsVue).forEach(([key, component]) => app.component(key, component as Component));
+
+app.use(Gapi, {
+  apiKey: import.meta.env.VITE_GAPI_API_KEY,
+  clientId: import.meta.env.VITE_GAPI_CLIENT_ID,
+  discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'],
+  scope: 'https://www.googleapis.com/auth/youtube.readonly',
+})
 
 app.mount('#app');
